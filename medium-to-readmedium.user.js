bf57eb912a5176ca877f62ecb958fae876e08481
alphaxleonidas <alphaxleonidas@gmail.com>
2026-06-25 02:25:59 +0500

    Add update and download URLs to user script
    Added update and download URLs for the script.

--- medium-to-readmedium.user.js
+++ medium-to-readmedium.user.js
@@ -3,6 +3,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Automatically redirects all medium.com pages to readmedium.com
+// @updateURL    https://github.com/alphaxleonidas/redirect-scripts/raw/refs/heads/main/medium-to-readmedium.user.js
+// @downloadURL  https://github.com/alphaxleonidas/redirect-scripts/raw/refs/heads/main/medium-to-readmedium.user.js
 // @author       Leonidas
 // @match        *://*.medium.com/*
 // @match        *://medium.com/*
